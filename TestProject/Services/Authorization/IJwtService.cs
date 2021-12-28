06cd1b3b26133f492eef476e0b6f6c8a9f3ae75d
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-12-28 19:00:52 +0200

    Add Authorization service

---
+++ TestProject/Services/Authorization/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using TestProject.Services.Authorization.Models;
+
+namespace TestProject.Services.Authorization
+{
+    public interface IJwtService
+    {
+        public string GetToken(JwtUser user);
+    }
+}
