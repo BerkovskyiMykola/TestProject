0fe26fadc8e916c15f72ee94293e08097469d001
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-12-31 20:44:02 +0200

    some changes

--- TestProject/ClientApp/src/components/NavMenu.js
+++ TestProject/ClientApp/src/components/NavMenu.js
@@ -22,50 +22,50 @@ const NavMenu = (props) => {
             >
                 <Container>
                     <NavbarBrand tag={Link} to="/">TestProject</NavbarBrand>
-                <NavbarToggler onClick={() => setCollapsed(!collapsed)} className="mr-2" />
-                <Collapse isOpen={collapsed} navbar>
-                    <Nav className="me-auto" navbar>
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                                {i18n.language.toUpperCase()}
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem onClick={() => i18n.changeLanguage("ua")}>
-                                    UA
-                                </DropdownItem>
-                                <DropdownItem onClick={() => i18n.changeLanguage("en")}>
-                                    EN
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <Nav className="d-flex" navbar>
-                        {user ? (
-                            <><NavItem>
-                                <NavLink tag={Link} to="/profile">{t("Profile")}</NavLink>
-                            </NavItem>
-                                {user.role === "Admin" &&
+                    <NavbarToggler onClick={() => setCollapsed(!collapsed)} className="mr-2" />
+                    <Collapse isOpen={collapsed} navbar>
+                        <Nav className="me-auto" navbar>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    {i18n.language.toUpperCase()}
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem onClick={() => i18n.changeLanguage("ua")}>
+                                        UA
+                                    </DropdownItem>
+                                    <DropdownItem onClick={() => i18n.changeLanguage("en")}>
+                                        EN
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                        <Nav className="d-flex" navbar>
+                            {user ? (
+                                <><NavItem>
+                                    <NavLink tag={Link} to="/profile">{t("Profile")}</NavLink>
+                                </NavItem>
+                                    {user.role === "Admin" &&
+                                        <NavItem>
+                                            <NavLink tag={Link} to="/users">{t("Users")}</NavLink>
+                                        </NavItem>
+                                    }
+                                    <li className="nav-item">
+                                        <a href="/login" className="nav-link" onClick={logOut}>
+                                            {t("LogOut")}
+                                        </a>
+                                    </li></>
+                            ) : (
+                                <>
                                     <NavItem>
-                                        <NavLink tag={Link} to="/users">{t("Users")}</NavLink>
+                                        <NavLink tag={Link} to="/login">{t("Login")}</NavLink>
                                     </NavItem>
-                                }
-                                <li className="nav-item">
-                                    <a href="/login" className="nav-link" onClick={logOut}>
-                                        {t("LogOut")}
-                                    </a>
-                                </li></>
-                        ) : (
-                            <>
-                                <NavItem>
-                                    <NavLink tag={Link} to="/login">{t("Login")}</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} to="/register">{t("SignUp")}</NavLink>
-                                </NavItem>
-                            </>
-                        )}
-                    </Nav>
-                </Collapse>
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/register">{t("SignUp")}</NavLink>
+                                    </NavItem>
+                                </>
+                            )}
+                        </Nav>
+                    </Collapse>
                 </Container>
             </Navbar>
         </header>
