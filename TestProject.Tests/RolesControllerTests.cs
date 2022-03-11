35c65966c2fae11928e05debbc3b826835b18acf
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-03-11 20:20:48 +0200

    remove unnecessary method

--- TestProject.Tests/RolesControllerTests.cs
+++ TestProject.Tests/RolesControllerTests.cs
@@ -10,27 +10,6 @@ namespace TestProject.Tests
     public class RolesControllerTests
     {
         [Fact]
-        public void GetRoles_Should_Use_Only_Admin()
-        {
-            //Arrage
-            var options = new DbContextOptionsBuilder<ApplicationContext>()
-                .UseInMemoryDatabase(databaseName: "RolesControllerDB_1")
-                .Options;
-
-            using var dbContext = new ApplicationContext(options);
-
-            var controller = new RolesController(dbContext);
-
-            //Act
-            var actualAttribute = controller.GetType()?.GetMethod("GetRoles")?.GetCustomAttributes(typeof(AuthorizeAttribute), true);
-
-            //Assert
-            var single = Assert.Single(actualAttribute);
-            var authorizeAttribute = Assert.IsType<AuthorizeAttribute>(single);
-            Assert.Equal("Admin", authorizeAttribute.Roles);
-        }
-
-        [Fact]
         public async void GetRoles_Should_Get_Roles()
         {
             //Arrage
